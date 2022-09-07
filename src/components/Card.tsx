1f1f6b2f60116ffc8341d538ff9c05d4c387809f
Hanz <22578473+fivehanz@users.noreply.github.com>
2022-09-07 18:15:37 +0530

    chore: card structure

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,10 +1,19 @@
 const Card = (props: TravelJournal) => {
-  const { id, name } = props;
+  const { id, name, date, description, country } = props;
 
   return (
-    <div className="card" key={id}>
-      {name}
-    </div>
+    <section className="card" key={id}>
+      {/* <img src="" alt="" className="card-image" /> */}
+      <div className="card-country">
+        <p className="card-country-title">{country}</p>
+        <a href="#" className="card-country-map-link">
+          View on Google Maps
+        </a>
+      </div>
+      <h1 className="card-title">{name}</h1>
+      <h2 className="card-date">{date}</h2>
+      <p className="card-desc">{description}</p>
+    </section>
   );
 };
 
